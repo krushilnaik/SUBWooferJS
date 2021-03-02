141301c95ad33f6ab2460a01ccf4b7f2347eeeb5
Krushil <krushilnaik96@gmail.com>
2021-03-02 15:18:50 -0500

    initialize most of the essential script files

---
+++ src/OverrideTag.js
@@ -0,0 +1,16 @@
+class OverrideTag {
+
+	/**
+	 * 
+	 * @param {string} rawText a single tag (with potential sub-tag parameters)
+	 * @param {OverrideTag[]} overloadings list of possible "function calls" for a tag if there's more than one
+	 */
+	constructor(rawText, overloadings=[]) {
+		this.name = overloadings !== [] ? overloadings[0].name : rawText;
+
+		this.parameters = [];
+		this.parenthetical = false;
+		this.commaSeperated = false;
+		this.isValid = false;
+	}
+}
