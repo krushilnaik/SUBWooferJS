ed1f3ad9f9ace953f799ccf4290be173bf4823f0
Krushil <krushilnaik96@gmail.com>
2021-03-02 20:34:58 -0500

    begin working on the override tag parser

--- src/OverrideTagParser.js
+++ src/OverrideTagParser.js
@@ -1,4 +1,65 @@
 const OverrideTag = require('../src/OverrideTag');
+const { VariableType } = require("../src/Enumerables");
+
+/**
+ * 
+ * @param {string} text 
+ * @param {string} delimiter 
+ */
+function splitTags(text, delimiter) {
+	/**
+	 * @type {string[]}
+	 */
+	let parameterList = [];
+
+	if (!text || text === "()") {
+		return parameterList;
+	}
+
+	if (text[0] !== "(") {
+		parameterList.push(text);
+		return parameterList;
+	}
+
+	let level = 0, prev = 1;
+	let removeIndex = -1;
+
+	for (let i = 1; i < text.length - 1; i++) {
+		const c = text[i];
+
+		if (c === "(") {
+			level += 1;
+		} else if (c === ")") {
+			level -= 1;
+
+			if (level === -1) {
+				removeIndex = i;
+				level += 1;
+			}
+		} else if (c === delimiter && level === 0) {
+			let temp = text.slice(prev, i);
+
+			if (removeIndex !== -1) {
+				temp = text.slice(prev, removeIndex);
+				removeIndex = -1;
+			}
+
+			prev = i;
+
+			parameterList.push(/,*(.*),*/g.exec(temp)[0]);
+		}
+	}
+
+	parameterList.push(/,*(.*),*/g.exec(text.slice(prev))[0]);
+
+	if (delimiter === "\\") {
+		while (parameterList[0].trim() === "") {
+			parameterList = parameterList.slice(1);
+		}
+	}
+
+	return parameterList;
+}
 
 class OverrideTagParser {
 	/**
@@ -20,6 +81,47 @@ class OverrideTagParser {
 		);
 
 		this.tagRegex = new RegExp(`(${this.validTags.join("|")}|\\(|\\))`).compile();
+	}
+
+
+	/**
+	 * Parse override tags in a string into OverrideTag and OverrideTagParameter objects
+	 * @param {string} text string of OverrideTags to parse
+	 */
+	generateTagsFromText(text) {
+		/**
+		 * @type {OverrideTag[]}
+		 */
+		let tags = [];
+
+		const strings = splitTags(`(${text})`, "\\");
+
+		for (const string of strings) {
+			let groups = string.split(this.tagRegex).filter(str => str.length !== 0);
+
+			const tagName = groups[0].replace("\\", "\\\\");
+			let isValidTag = true;
+
+			if (!this.validTags.includes(tagName)) {
+				isValidTag = false;
+			}
+
+			if (isValidTag) {
+				const overrides = this.tagMap.get(tagName);
+
+				try {
+					tags.push(new OverrideTag(string, overrides));
+
+					for (let i = 0; i < tags[tags.length - 1].parameters.length; i++) {
+						if (tags[tags.length - 1].parameters[i] === VariableType.OVERRIDE) {
+							// 
+						}
+					}
+				} catch (error) {
+					continue;
+				}
+			}
+		}
 	}
 }
 
